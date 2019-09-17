858902f34a4bc4e994a6f91ad5b1ab4071e665d6
mishu <mishu@xtelecom.ro>
2019-09-17 13:14:14 +0300

    removes @click.stop

--- src/bulma/VueSwitch.vue
+++ src/bulma/VueSwitch.vue
@@ -4,7 +4,6 @@
         <input class="checkbox"
             type="checkbox"
             v-model="checked"
-            @click.stop
             @keydown.prevent.enter="$refs.input.click()"
             @change="$emit('input', checked)"
             :disabled="disabled || readonly"
